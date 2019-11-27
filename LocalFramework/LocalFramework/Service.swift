1d418f78f82564e503d3c98023e744c88c84b09b
Home Computer <macmini@macmini-homes-Mac-mini.local>
2019-11-27 11:29:26 +0530

    Finished our LocalFramework

---
+++ LocalFramework/LocalFramework/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  LocalFramework
+//
+//  Created by Home Computer on 27/11/2019.
+//  Copyright © 2019 Home Computer. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() { }
+    
+    public static func doSomething() -> String {
+        return "You did something, here you go!!!"
+    }
+}
